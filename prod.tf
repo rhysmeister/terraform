3a1588671fd67535a25fa99222c66c4cbacf62ac
Rhys Campbell <rhys.james.campbell@googlemail.com>
2021-06-28 16:12:02 +0200

    Rename file

--- prod.tf
+++ prod.tf
@@ -3,7 +3,11 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
-resource "aws_s3_bucket" "tf_course_bucket" {
+resource "aws_s3_bucket" "prod_tf_course_bucket" {
   bucket = "tf-course-hdjkdhfkjasgdfafsdhja"
   acl    = "private"
 }
+
+resource "aws_default_vpc" "default" {}
+
+
